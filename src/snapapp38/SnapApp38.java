77642f511c086c800fcd660e45e5886b3107f51c
yuzshofar <yuzshofar@gmail.com>
2018-10-29 11:27:12 +0700

    upload pertama

---
+++ src/snapapp38/SnapApp38.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snapapp38;
+
+/**
+ *
+ * @author Khairuzzain
+ */
+public class SnapApp38 extends SnapFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapApp38 x = new SnapApp38();
+        x.setVisible (true);
+    }
+}
